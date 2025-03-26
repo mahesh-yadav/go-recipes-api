fd1ed4c8173e3941bec8f71e4750f59090f4a4f0
Mahesh Yadav <mahesh.iitmandi@gmail.com>
2025-03-26 10:03:08 +0530

    new recipe endpoint

--- server.go
+++ server.go
@@ -1,13 +1,23 @@
 package main
 
 import (
+	"net/http"
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/rs/xid"
 )
+
+// In-Memory DB
+var recipes []Recipe
+
+func initDb() {
+	recipes = make([]Recipe, 0)
+}
 
 // Data Models
 type Recipe struct {
+	ID           string    `json:"id"`
 	Name         string    `json:"name"`
 	Tags         []string  `json:"tags"`
 	Ingredients  []string  `json:"ingredients"`
@@ -15,7 +25,27 @@ type Recipe struct {
 	PublishedAt  time.Time `json:"published_at"`
 }
 
+// Route Handlers
+func NewRecipeHandler(c *gin.Context) {
+	var recipe Recipe
+	if err := c.ShouldBindJSON(&recipe); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	recipe.ID = xid.New().String()
+	recipe.PublishedAt = time.Now()
+
+	recipes = append(recipes, recipe)
+	c.JSON(http.StatusCreated, recipe)
+}
+
 func main() {
 	router := gin.Default()
+
+	router.POST("/recipes", NewRecipeHandler)
+
 	router.Run()
 }
